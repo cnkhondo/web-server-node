36f86d9c080e54bf2cc4cbb837c2f764004603df
Nkhondo Chirwa <nkhondo@Nkhondos-MacBook-Pro.local>
2018-07-21 09:52:50 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const fs = require('fs');
+const hbs = require('hbs');
+
+const app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if(err) {
+            console.log('Unable to append to server log.');
+        }
+    });
+    next();
+});
+
+app.use((req, res, next) => {
+    res.render('maintenance');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+    res.render('home', {
+        title: 'Home Page',
+        message: 'Welcome to the Home Page!'
+    });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Page',
+    });
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Unable to fulfill this request.'
+    });
+});
+
+app.listen(7777, () => {
+    console.log('Server is up on port 3000');
+});
